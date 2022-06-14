70054dcc5f8b107536d1a505bb530564a939595e
lam268 <lamlevu26@gmail.com>
2022-06-14 15:52:55 +0700

    fixbugs

--- contracts/Project.sol
+++ contracts/Project.sol
@@ -1,13 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
-import "./Whitelist.sol";
 
 contract Token is ERC721 {
     struct Project {
         string name;
         string symbol;
+        string url;
         uint256 number;
     }
     mapping(uint256 => Project) public projectList;
@@ -21,12 +20,14 @@ contract Token is ERC721 {
         address _to,
         uint256 id,
         string memory name,
-        string memory symbol
+        string memory symbol,
+        string memory url,
     ) public {
         _safeMint(_to, id);
         projectList[id].name = name;
         projectList[id].symbol = symbol;
         projectList[id].number = id;
+        projectList[id].url = url;
     }
 
     function getProjectInformation(uint256 id)
